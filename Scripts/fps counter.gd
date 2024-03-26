16eb73aceabb6b3c155b3510cb8ba14da1320b05
5Art <asheanx@gmail.com>
2024-03-27 00:52:17 +0100

    dithering

--- Scripts/fps counter.gd
+++ Scripts/fps counter.gd
@@ -9,3 +9,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	text = str(Engine.get_frames_per_second()) + " fps";
+
+func mario():
+	randomize()
