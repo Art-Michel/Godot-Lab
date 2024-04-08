f2af8ca5f06324f2a4f25a867dc8bb7b2b6365b2
5Art <asheanx@gmail.com>
2024-04-08 19:04:56 +0200

    omg le plugin

--- addons/flowerwalls_crt/crt.gd
+++ addons/flowerwalls_crt/crt.gd
@@ -3,9 +3,9 @@ extends Control
 var crt_pp_packed = preload("crt_pp.tscn")
 var crt_pp: Control
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.is_pressed():
-		if event is InputEventKey.KEY_F2:
+func _unhandled_key_input(event):
+	if event is InputEventKey and !event.is_echo() and event.is_pressed():
+		if event.keycode == KEY_F3:
 			enable_crt()
 
 func enable_crt() -> void:
