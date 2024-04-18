f5cbbd8c44e99cbde820f58ecadceb0b0400f0b4
5Art <asheanx@gmail.com>
2024-04-18 23:47:09 +0200

    boooon pause

--- addons/flowerwall_postprocess/flowerwall_pp_script.gd
+++ addons/flowerwall_postprocess/flowerwall_pp_script.gd
@@ -11,10 +11,34 @@ extends Control
 
 
 func _ready():
-	print (crt_shader.get("shader_parameter/noise_strength"))
-	print (crt_shader.get("shader_parameter/scanline_opacity"))
+	$crt_toggle.connect("toggled", _on_crt_toggled)
+	$preblur_toggle.connect("toggled", _on_preblur_toggled)
+	$dithering_toggle.connect("toggled", _on_dithering_toggled)
 	$noise_slider.connect("value_changed", _on_noise_changed)
 	$scanlines_slider.connect("value_changed", _on_scanlines_changed)
+	$downscale_slider.connect("value_changed", _on_downscale_changed)
+	$curvature_slider.connect("value_changed", _on_curvature_changed)
+
+
+func _on_crt_toggled(value:bool) -> void:
+	crt.visible = value
+	bloom.visible = value
+
+func _on_preblur_toggled(value:bool) -> void:
+	preblur_x.visible = value
+	preblur_y.visible = value
+
+func _on_dithering_toggled(value:bool) -> void:
+	dither_shader.set("shader_parameter/dithering", value)
+	var coldep: float
+	if value: coldep = 4
+	else: coldep = 8
+	dither_shader.set("shader_parameter/color_depth", coldep)
+
+
+func _on_downscale_changed(value:float) -> void:
+	dither_shader.set("shader_parameter/resolution_scale", 1/value)
+	get_window().scaling_3d_scale = value
 
 
 func _on_noise_changed(value:float) -> void:
@@ -33,3 +57,7 @@ func update_brightness() -> void:
 	
 	var value: float = 0.8 + sc_opacity * 0.5 + noise_str / 1.5
 	crt_shader.set("shader_parameter/brightness_multiplier", value)
+
+
+func _on_curvature_changed(value: float) -> void:
+	crt_shader.set("shader_parameter/curve_power", value)
